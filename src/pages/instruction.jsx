166e8a4d82a20d4a2350c83a6eb64309d6bcd959
Taikai Vu <vudangdaiduong@gmail.com>
2024-06-16 18:22:48 +0700

    config fix

--- src/pages/instruction.jsx
+++ src/pages/instruction.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Nav from "../component/NavBar";
 import Footer from "../component/Footer";
 
 
 const InsView = (props) => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+      }, []);
+
     return (
         <div>
             <Nav />
